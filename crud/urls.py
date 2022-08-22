376505b89b8b96e132e3c7577a8bdc6ef531247b
AminAlih47 <AminAlih47@gmail.com>
2022-08-22 21:30:21 +0430

    ✅ Code formatted!

--- crud/urls.py
+++ crud/urls.py
@@ -4,9 +4,9 @@ from crud import views
 app_name = "crud"
 
 urlpatterns = (
-    path('', views.UsersList.as_view(), name="list"),
-    path('<int:pk>/', views.RetrieveUser.as_view(), name="retrieve"),
-    path('create/', views.CreateUser.as_view(), name="create"),
-    path('update/<int:pk>/', views.UpdateUser.as_view(), name="update"),
-    path('delete/<int:pk>/', views.DeleteUser.as_view(), name="delete"),
+    path("", views.UsersList.as_view(), name="list"),
+    path("<int:pk>/", views.RetrieveUser.as_view(), name="retrieve"),
+    path("create/", views.CreateUser.as_view(), name="create"),
+    path("update/<int:pk>/", views.UpdateUser.as_view(), name="update"),
+    path("delete/<int:pk>/", views.DeleteUser.as_view(), name="delete"),
 )
